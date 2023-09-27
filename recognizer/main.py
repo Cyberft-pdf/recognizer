32958c7f1fb53543cc8c2d9c1989ee1e05c42cb6
Adel <adi.stein@outlook.cz>
2023-09-27 22:10:09 +0200

    buttons, loading screen
    Working loading screen, 2 buttons,
    
    I add new function
    
    graphics is going to be change

--- recognizer/main.py
+++ recognizer/main.py
@@ -5,12 +5,15 @@ import json
 from datetime import datetime
 import pygame
 import sys
+import subprocess
+import platform
+
 
 pygame.init()
 #Základní nastavení - začátek
 WIDTH, HEIGHT = 1200,800
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption("Planet Simulation")
+pygame.display.set_caption("Recognizer")
 
 #Základní nastavení- konec
 pygame.init()
@@ -22,11 +25,14 @@ font = pygame.font.SysFont(None, 24)
 clock = pygame.time.Clock()
 
 
+
 #obrázky - začátek
+backround_image = pygame.image.load("pictures/backround_image.png").convert_alpha()
 start_img = pygame.image.load("pictures/start_button_face.png").convert_alpha()
-exit_img = pygame.image.load("pictures/start_image.png").convert_alpha()
+button_img_net = pygame.image.load("pictures/button_net.png").convert_alpha()
+button_image_tr2 = pygame.transform.scale(backround_image, (1200, 800))
 button_image_tr1 = pygame.transform.scale(start_img, (220, 145))
-button_image_tr2 = pygame.transform.scale(exit_img, (1200, 800))
+button_image_tr3 = pygame.transform.scale(button_img_net, (220, 145))
 #obrázky - konec
 
 #velikost tlačítka - začátek
@@ -34,12 +40,19 @@ button_width = 190
 button_widt2 = 400
 button_height = 115
 button_height2 = 600
+
 button_x = (button_height) // 1
 button_y = (button_height2) // 1
-button_rect = pygame.Rect(button_x, button_y, button_width, button_height)
 
-start_button_rect = start_img.get_rect()
-start_button_rect2 = exit_img.get_rect()
+button_x2 =  400
+button_y2 =  590
+
+button_rect = pygame.Rect(button_x, button_y, button_width, button_height)
+button_rect2 = pygame.Rect(button_x2, button_y2, button_width, button_height)
+
+
+start_button_rect = button_img_net.get_rect()
+start_button_rect2 = button_img_net.get_rect()
 #velikost tlačítka - konec
 
 
@@ -57,7 +70,8 @@ def recognizer():
 
     cap = cv2.VideoCapture(0)
 
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
 
 
     while True:
@@ -111,10 +125,10 @@ def recognizer():
             cv2.putText(frame, f"Age: {age}", (x, y-30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
 
-        cv2.imshow('Live Detekce a Rozpoznávání Pohlaví', frame)
+        cv2.imshow("Live Detekce a Rozpoznávání Pohlaví", frame)
 
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             with open("data.json", "a") as json_file:
                 json.dump(data_list, json_file, indent=4)
                 json_file.write("\n")
@@ -123,6 +137,35 @@ def recognizer():
 
     cap.release()
     cv2.destroyAllWindows()
+
+
+
+
+def my_network_information():
+    command = "'color 2 & ipconfig /all" 
+    command = f"color 2 & {command}"
+
+    subprocess.Popen(["start", "cmd", "/k", command], shell=True)
+
+
+
+    try:
+        result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        
+        output = result.stdout
+        error = result.stderr
+        
+        print("Výstup z příkazu:")
+        print(output)
+        
+        if error:
+            print("Chyba:")
+            print(error)
+    except subprocess.CalledProcessError as e:
+        print(f"Chyba při spuštění příkazu: {e}")
+
+
+
 
 #hlavní while loop
 show_intro = True
@@ -142,16 +185,24 @@ while running:
 
             if current_time - intro_start_time >= intro_duration:
                 show_intro = False
-    #Pokračování na simulaci
+
     else:
-        WIN.blit(exit_img, (0, 0))        
+        WIN.blit(button_image_tr2, (0, 0))
+        WIN.blit(button_image_tr3, button_rect2,)                    
         WIN.blit(button_image_tr1, button_rect,)
         mouse_pos = pygame.mouse.get_pos()
         mouse_pressed = pygame.mouse.get_pressed()
         if button_rect.collidepoint(mouse_pos) and mouse_pressed[0]:
-                pygame.quit()                
-                recognizer()
+            pygame.quit()  
+            recognizer()
+
+        elif button_rect2.collidepoint(mouse_pos) and mouse_pressed[0]:
+            pygame.quit()  
+            my_network_information()
+
+
     
+
     pygame.display.flip()
 
 pygame.quit()
