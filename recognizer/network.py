2ea1d6568c3f2a988584b1bfe426d43b5175b62f
Adel <adi.stein@outlook.cz>
2023-10-07 18:52:06 +0200

    Update network.py

--- recognizer/network.py
+++ recognizer/network.py
@@ -1,25 +1,4 @@
-import subprocess
-import platform
+import pandas
+import matplotlib
 
 
-command = "'color 2 & ipconfig /all" 
-command = f'color 2 & {command}'
-
-subprocess.Popen(["start", "cmd", "/k", command], shell=True)
-
-
-
-try:
-    result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    
-    output = result.stdout
-    error = result.stderr
-    
-    print("Výstup z příkazu:")
-    print(output)
-    
-    if error:
-        print("Chyba:")
-        print(error)
-except subprocess.CalledProcessError as e:
-    print(f"Chyba při spuštění příkazu: {e}")
