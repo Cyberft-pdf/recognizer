7cbcc8ed8c75bc4941945b111955e43a87ee81b9
Adel <80000156+Cyberft-pdf@users.noreply.github.com>
2023-11-21 09:20:57 +0100

    Update main.py

--- recognizer/main.py
+++ recognizer/main.py
@@ -188,6 +188,28 @@ def wifi_okoli():
         print("-------------------------------------")   
 #NENÍ ZPOVOZNĚNO 
 
+#NENÍ ZPOVOZNĚNO 
+
+def gen_pasword():
+    site = input("What website or app:")
+    
+    def generate_password(length=12):
+        characters = string.ascii_letters + string.digits + string.punctuation
+        password = "".join(random.choice(characters) for i in range(length))
+        return password
+    
+    generate_password = generate_password()
+    
+    with open("data.txt", "a") as f:
+        data = site , generate_password
+        f.write( "\n")
+        f.write(str(data))
+        f.write("\n")
+    
+    
+    print(f"New password is: {generate_password}")
+
+#NENÍ ZPOVOZNĚNO 
 
 #hlavní while loop
 show_intro = True
